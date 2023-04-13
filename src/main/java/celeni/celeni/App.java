bd62b888bea9bb445fd6567b8622f07aa79fc961
jays <jaysrozario@gmail.com>
2023-04-13 13:37:10 -0400

    selenium

---
+++ src/main/java/celeni/celeni/App.java
@@ -0,0 +1,13 @@
+package celeni.celeni;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
